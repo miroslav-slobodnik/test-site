5ab58c02cb05a4b8886cd6dbb76f1764469eecc1
Miroslav Slobodnik <MSlobodnik@gohealth.com>
2021-01-09 04:38:49 +0100

    Color change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={ { backgroundColor: '#fff0f0'} }>
       <header className="App-header">
         <h1>Hello Miroslav and Alzbetka :)</h1>
       </header>
